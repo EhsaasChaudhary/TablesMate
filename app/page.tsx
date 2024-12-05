03129c8975fac255fa270b299b10a142f2467399
EhsaasChaudhary <116794955+EhsaasChaudhary@users.noreply.github.com>
2024-12-06 01:17:35 +0530

    adding functionality to add multiple table
    still work in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 
 import { TableDataForm } from "./components/dynamictableform"
 
+// import InputForm from "./components/dynamictableform";
 
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Dynamic Table</h1>
       <TableDataForm/>
+      {/* <InputForm/> */}
     </main>
   )
 }
